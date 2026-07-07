45db6fc5c067b7dd754071e380337def10820049
Mihai897 <mihai897@yahoo.com>
2026-07-07 12:23:43 +0300

    Neon connection

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,7 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = new Pool({
+const pool =process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    })
+  : new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
